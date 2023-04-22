cd48306c6f6d5e34b0dfda4e4fd99c04c0d18580
Fabian <fvreede@me.com>
2023-04-22 23:59:55 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,39 @@
+#install nix
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+#source nix
+ . /home/vreedfab/.nix-profile/etc/profile.d/nix.sh
+
+#install nix packages
+
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.neovim \
+	nixpkgs.packer \
+	nixpkgs.git \
+	nixpkgs.bat \
+	nixpkgs.fzf \
+	nixpkgs.antibody \
+	nixpkgs.mc \
+    nixpkgs.python3Full \
+	nixpkgs.stow \
+    nixpkgs.spaceship-prompt \
+	nixpkgs.ripgrep
+
+#add zsh to valid shells
+command -v zsh | sudo tee -a /etc/shells
+
+#make zsh default shell for user
+#if PAM problems, sudo nano /etc/pam.d/chsh and comment out "accounts with special shells from changing them. auth       sufficient   pam_shells.so
+sudo chsh -s $(which zsh) $USER
+
+#load antibody plugins
+antibody bundle < ~/.zsh_plugins.txt
+
+#stow .dotfiles
+stow zsh
+stow nvim
+stow mc
+
+source .zshrc
+
---
+++ zsh/.zsh_plugins.sh
@@ -0,0 +1,23 @@
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-spaceship-prompt-SLASH-spaceship-prompt/async.zsh
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-spaceship-prompt-SLASH-spaceship-prompt/spaceship.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-spaceship-prompt-SLASH-spaceship-prompt )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/command-not-found )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git-extras/git-extras.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git-extras )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/node/node.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/node )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm/zsh-nvm.plugin.zsh
+fpath+=( /home/vreedfab/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm )
